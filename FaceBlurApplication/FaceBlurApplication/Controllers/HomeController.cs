ac1a9c4814011f39ea1d89c012f74e918b9607e8
Gleb <gleb.mirolyubov@gmail.com>
2019-10-25 22:47:02 +0400

    Finished main functionality

--- FaceBlurApplication/FaceBlurApplication/Controllers/HomeController.cs
+++ FaceBlurApplication/FaceBlurApplication/Controllers/HomeController.cs
@@ -25,10 +25,8 @@ namespace FaceBlurApplication.Controllers
         private readonly IWebHostEnvironment _he;
         private string _contentString;
 
-        // Replace <Subscription Key> with your valid subscription key.
         const string subscriptionKey = "eabdfe0844ed4e0f89570260183ab6a7";
 
-        // replace <myresourcename> with the string found in your endpoint URL
         const string uriBase =
             "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
 
@@ -72,7 +70,9 @@ namespace FaceBlurApplication.Controllers
                 {
                     Task t = MakeAnalysisRequest(fileName);
                     t.Wait();
+
                     BlurFaces(GetFaceRectangles(_contentString), fileName);
+
                     Console.WriteLine("\nWait a moment for the results to appear.\n");
                 }
                 catch (Exception e)
@@ -80,49 +80,40 @@ namespace FaceBlurApplication.Controllers
                     Console.WriteLine("\n" + e.Message + "\nPress Enter to exit...\n");
                 }
             }
+
             return View();
         }
 
-        // Gets the analysis of the specified image by using the Face REST API.
         public async Task MakeAnalysisRequest(string imageFilePath)
         {
             HttpClient client = new HttpClient();
 
-            // Request headers.
             client.DefaultRequestHeaders.Add(
                 "Ocp-Apim-Subscription-Key", subscriptionKey);
 
-            // Request parameters. A third optional parameter is "details".
             string requestParameters = "returnFaceId=true&returnFaceLandmarks=false" +
                 "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses," +
                 "emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
 
-            // Assemble the URI for the REST API Call.
             string uri = uriBase + "?" + requestParameters;
 
             HttpResponseMessage response;
 
-            // Request body. Posts a locally stored JPEG image.
             byte[] byteData = GetImageAsByteArray(imageFilePath);
 
             using (ByteArrayContent content = new ByteArrayContent(byteData))
             {
-                // This example uses content type "application/octet-stream".
-                // The other content types you can use are "application/json"
-                // and "multipart/form-data".
                 content.Headers.ContentType =
                     new MediaTypeHeaderValue("application/octet-stream");
 
-                // Execute the REST API call.
                 response = await client.PostAsync(uri, content);
 
-                // Get the JSON response.
                 string contentString = await response.Content.ReadAsStringAsync();
                 _contentString = contentString;
             }
         }
 
-        // Returns the contents of the specified file as a byte array.
+
         static byte[] GetImageAsByteArray(string imageFilePath)
         {
             using (FileStream fileStream =
@@ -133,19 +124,21 @@ namespace FaceBlurApplication.Controllers
             }
         }
 
-        static JArray GetFaceRectangles(string contentString)
+        public JArray GetFaceRectangles(string contentString)
         {
+            ViewData["FaceCount"] = "NoFaces";
             var data = JArray.Parse(contentString);
             var faceRectangles = new JArray();
 
             if (data.Count > 0)
             {
+                ViewData["FaceCount"] = "Faces";
                 for (int i = 0; i < data.Count; i++)
                 {
                     faceRectangles.Add(data[i]["faceRectangle"]);                   
                 }
             }
-
+            
             return faceRectangles;
         }
 
@@ -168,7 +161,7 @@ namespace FaceBlurApplication.Controllers
             string filePath = Path.Combine(_he.WebRootPath, "blurred.jpg");
 
             bitmap.Save(filePath);
-
+   
             ViewData["blurredImage"] = "/blurred.jpg";
         }
         private static Bitmap Blur(Bitmap image, Int32 blurSize, Rectangle rectToBlur)
@@ -178,10 +171,9 @@ namespace FaceBlurApplication.Controllers
 
         private static Bitmap Blur(Bitmap image, Rectangle rectangle, Int32 blurSize)
         {
-            Bitmap blurred = new Bitmap(image);   //image.Width, image.Height);
+            Bitmap blurred = new Bitmap(image); 
             using (Graphics graphics = Graphics.FromImage(blurred))
             {
-                // look at every pixel in the blur rectangle
                 for (Int32 xx = rectangle.Left; xx < rectangle.Right; xx += blurSize)
                 {
                     for (Int32 yy = rectangle.Top; yy < rectangle.Bottom; yy += blurSize)
@@ -190,8 +182,6 @@ namespace FaceBlurApplication.Controllers
                         Int32 blurPixelCount = 0;
                         Rectangle currentRect = new Rectangle(xx, yy, blurSize, blurSize);
 
-                        // average the color of the red, green and blue for each pixel in the
-                        // blur size while making sure you don't go outside the image bounds
                         for (Int32 x = currentRect.Left; (x < currentRect.Right && x < image.Width); x++)
                         {
                             for (Int32 y = currentRect.Top; (y < currentRect.Bottom && y < image.Height); y++)
@@ -210,7 +200,6 @@ namespace FaceBlurApplication.Controllers
                         avgG = avgG / blurPixelCount;
                         avgB = avgB / blurPixelCount;
 
-                        // now that we know the average for the blur size, set each pixel to that color
                         graphics.FillRectangle(new SolidBrush(Color.FromArgb(avgR, avgG, avgB)), currentRect);
                     }
                 }
