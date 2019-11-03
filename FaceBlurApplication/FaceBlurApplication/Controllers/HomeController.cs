26b1131a034824fcc34f7316a14e4b887ef8dcd2
Gleb <gleb.mirolyubov@gmail.com>
2019-11-03 16:12:24 +0400

    First workable version of face blur on websites

--- FaceBlurApplication/FaceBlurApplication/Controllers/HomeController.cs
+++ FaceBlurApplication/FaceBlurApplication/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.IO;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,13 +10,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using FaceBlurApplication.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Json.Net;
 using Newtonsoft.Json.Linq;
 using System.Drawing;
 using HtmlAgilityPack;
-using System.Net;
-using System.Web;
 
 namespace FaceBlurApplication.Controllers
 {
@@ -27,6 +21,7 @@ namespace FaceBlurApplication.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IWebHostEnvironment _he;
         private string _contentString;
+        private int blurCount = 0;
 
         const string subscriptionKey = "3d8c0a687bb64a27903b162a2badaf89";
 
@@ -61,10 +56,6 @@ namespace FaceBlurApplication.Controllers
             var response = await client.GetAsync(url);
             var pageContents = await response.Content.ReadAsStringAsync();
 
-            //Console.WriteLine(pageContents);
-
-            //ViewData["LoadedWebpage"] = pageContents;
-
             GetAllImages(pageContents, url);
         }
 
@@ -81,37 +72,6 @@ namespace FaceBlurApplication.Controllers
             var baseUri = uri.GetLeftPart(System.UriPartial.Authority);
             int count = 0;
 
-            /*
-            int count = 0;
-
-
-            foreach (var item in urls)
-            {
-                if (CheckURLCorrect(item) == true)
-                {
-                    System.Drawing.Image image = DownloadImageFromUrl(item);
-
-                    string rootPath = _he.WebRootPath;
-                    string fileName = System.IO.Path.Combine(rootPath, count+".jpg");
-                    image.Save(fileName);
-                    count++;
-
-                    //Task t = MakeAnalysisRequest(fileName);
-                    //t.Wait();
-
-                    //BlurFaces(GetFaceRectangles(_contentString), fileName);
-                } else
-                {
-                    string absoluteURL = baseUri + item;
-
-                    if (CheckURLCorrect(absoluteURL) == true)
-                    
-                        Console.WriteLine("CORRECT!: "+absoluteURL);                  
-                }
-            }
-            */
-
-
             foreach (HtmlNode node in document.DocumentNode.SelectNodes("//img[@src]"))
             {
                 var src = node.Attributes["src"].Value.Split('?');
@@ -127,14 +87,19 @@ namespace FaceBlurApplication.Controllers
                         image.Save(fileName);
                         count++;
 
-                        //Task t = MakeAnalysisRequest(fileName);
-                        //t.Wait();
+                        Task t = MakeAnalysisRequest(fileName);
+                        t.Wait();
 
-                        //BlurFaces(GetFaceRectangles(_contentString), fileName);
+                        if (GetFaceRectangles(_contentString).Count > 0)
+                        {
+                            fileName = BlurFaces(GetFaceRectangles(_contentString), fileName);
+
+                            node.SetAttributeValue("src", "https://localhost:5001/"+fileName);
+                        }
                     }
                     catch (Exception e)
                     {
-
+                        Console.WriteLine(e.Message);
                     }
                 } 
                 else
@@ -150,19 +115,24 @@ namespace FaceBlurApplication.Controllers
                         string fileName = System.IO.Path.Combine(rootPath, count + ".jpg");
                         image.Save(fileName);
                         count++;
+
+                        Task t = MakeAnalysisRequest(fileName);
+                        t.Wait();
+
+                        if (GetFaceRectangles(_contentString).Count > 0)
+                        {
+                            fileName = BlurFaces(GetFaceRectangles(_contentString), fileName);
+
+                            node.SetAttributeValue("src", "https://localhost:5001/" + fileName);
+                        }
                     } 
                     catch( Exception e)
                     {
                         Console.WriteLine(e.Message);
                     } 
                 }
-
-                //node.SetAttributeValue("src", "https://sun9-63.userapi.com/c637928/v637928334/1a2f7/6Yqr9p7PyXE.jpg");
-
-                //https://www.goethe.de/en/uun/org/pra.html USE THIS URL AS TEST
             }
             
-
             ViewData["LoadedWebpage"] = document.DocumentNode.OuterHtml;
         }
 
@@ -203,47 +173,16 @@ namespace FaceBlurApplication.Controllers
 
         public IActionResult WebpageWithBlurredFaces (string url)
         {
-            Task t = LoadWebpage(url);
-            t.Wait();
-
-            return View();
-        }
-
-        public void UploadImage(IFormFile img)
-        {
-            if (img != null)
+            try
             {
-                var fileName = Path.Combine(_he.WebRootPath, Path.GetFileName(img.FileName));
-
-                try
-                {
-                    using (FileStream fileStream = new FileStream(fileName, FileMode.Create))
-                    {
-                        img.CopyTo(fileStream);
-                    }
-                }
-                catch (Exception e)
-                {
-                    ViewData["FaceCount"] = "NoFaces";
-                }
-
-                ViewData["initialImage"] = "/" + Path.GetFileName(img.FileName);
-                ViewData["Arrow"] = "/arrow.png";
-
-                try
-                {
-                    Task t = MakeAnalysisRequest(fileName);
-                    t.Wait();
-
-                    BlurFaces(GetFaceRectangles(_contentString), fileName);
-
-                    Console.WriteLine("\nWait a moment for the results to appear.\n");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("\n" + e.Message + "\nPress Enter to exit...\n");
-                }
+                Task t = LoadWebpage(url);
+                t.Wait();
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                ViewData["LoadedWebpage"] = "ERROR";
             }
+            return View();
         }
 
         public async Task MakeAnalysisRequest(string imageFilePath)
@@ -275,7 +214,6 @@ namespace FaceBlurApplication.Controllers
             }
         }
 
-
         static byte[] GetImageAsByteArray(string imageFilePath)
         {
             using (FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
@@ -303,7 +241,7 @@ namespace FaceBlurApplication.Controllers
             return faceRectangles;
         }
 
-        public void BlurFaces(JArray facesArray, string path)
+        public string BlurFaces(JArray facesArray, string path)
         {
             Bitmap bitmap = new Bitmap(path);
    
@@ -319,12 +257,18 @@ namespace FaceBlurApplication.Controllers
                 bitmap = Blur(bitmap, 20, rectangle);
             }
 
-            string filePath = Path.Combine(_he.WebRootPath, "blurred.jpg");
+            string filePath = Path.Combine(_he.WebRootPath, blurCount+"blurred.jpg");
+            string fileName = blurCount + "blurred.jpg";
+            blurCount++;
 
             bitmap.Save(filePath);
-   
+
             ViewData["blurredImage"] = "/blurred.jpg";
+
+            return fileName;
+  
         }
+
         private static Bitmap Blur(Bitmap image, Int32 blurSize, Rectangle rectToBlur)
         {
             return Blur(image, new Rectangle(rectToBlur.Top, rectToBlur.Left, rectToBlur.Width, rectToBlur.Height), blurSize);
